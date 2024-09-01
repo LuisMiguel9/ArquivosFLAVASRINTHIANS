e0bcd81d5f44ebd84a1320bd7d437b65f9ec142d
Luis Henrique <143910163+luisRibeiro23@users.noreply.github.com>
2024-09-01 00:27:58 -0300

    Add files via upload

---
+++ java/AngryBirds.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class AngryBirds {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double v = scan.nextDouble();
+        double ag = scan.nextDouble();
+        double d = scan.nextDouble();
+        double radians = Math.toRadians(ag);
+        double r = ((v*v)*(Math.sin(radians*2)))/9.8;
+        if(d-r<=0.1){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+    }
+}
