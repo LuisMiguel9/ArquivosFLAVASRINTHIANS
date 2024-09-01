c19189ffae5041d83b65df94e57b8c2b406d4516
Luis Henrique <143910163+luisRibeiro23@users.noreply.github.com>
2024-09-01 01:19:10 -0300

    Delete java/PinturaMuro.java

--- java/PinturaMuro.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class PinturaMuro {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        float v = scan.nextFloat();
-        float c = (v*36)+100;
-        System.out.printf("%.1f",c);
-    }
-}
